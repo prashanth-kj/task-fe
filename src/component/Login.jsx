d622ef75a7374c24f4f14a582eca5f0670ad6703
prashanth-kj <kjprasanth98@gmail.com>
2024-07-27 09:05:32 +0530

    updated

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,6 +2,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import AxiosService from '../utils/Apiservice';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 function Login() {
           
@@ -41,8 +42,14 @@ function Login() {
                 <Form.Label className="form-label">Password</Form.Label>
                 <Form.Control type="text" className="form-control" placeholder="Enter your password" onChange={(e)=>setPassword(e.target.value)}/>
             </Form.Group>
-            <Button variant="primary" className='w-100' onClick={()=>handleLogin()}>Login</Button>
+           <Button variant="primary" className='w-100' onClick={()=>handleLogin()}>Login</Button>
+        
+             <Form.Group className="mb-3 text-center mt-5">
+                      <Form.Label>Don't have an Account? <Link to={'/register'} style={{textDecoration:"none"}}>Register</Link></Form.Label>
+             </Form.Group>
         </Form>
+         
+        
      </div>
   </>
 }
